5c4dbe7e0e36e6b4b4e9403268dbc4efcebac154
Roso Peñaranda <rosopenaranda@gmail.com>
2022-08-27 12:05:44 -0500

    added services

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,19 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { AppConfigModule } from './config/app/config.module';
+import { DatabaseConfigModule } from './config/database/config.module';
+import { TypeOrmConfigModule } from './config/typeorm/typeorm.module';
+import { User } from './database/entities/user.entity';
 
 @Module({
-  imports: [],
+  imports: [
+    TypeOrmConfigModule,
+    TypeOrmModule.forFeature([User]),
+    DatabaseConfigModule,
+    AppConfigModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
